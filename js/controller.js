60b673fa9e4f63aea9440f7d47dffd3d6c1e9f94
JJ Berrett <developer@jjberrett.com>
2017-08-11 01:53:16 -0600

    Added computer AI

--- js/controller.js
+++ js/controller.js
@@ -9,19 +9,23 @@ function createTable(height, width) {
 	// createTableArray(HEIGHT, WIDTH);
 	updateComputerTable();
 	updateUserTable();
+	let userName = localStorage.getItem('username');
+	document.getElementById('playerName').innerHTML = userName + "'s Grid";
 }
 
 
-function makeTableInteractive() {
-	let cells = document.getElementsByTagName("td");
+function makeTableInteractive(id) {
+	let div = document.getElementById(id);
+	let cells = div.getElementsByTagName("td");
 	for (let i = 0; i < cells.length; i++) {
 		cells[i].onclick = checkCoordinates;
 	}
 }
 
+
 function updateComputerTable() {
-	let div = document.getElementById('computer');
-	let tableHtml = '<table class="table" id="computer">';
+	let div = document.getElementById('computer-grid');
+	let tableHtml = '<table class="table" align="center" id="computer">';
 
 	for (let i = 0; i < WIDTH; i++) {
 		tableHtml += '<tr>';
@@ -29,7 +33,7 @@ function updateComputerTable() {
 			let cell = players.computer.table[i][j];
 
 			if (cell !== 'hit' && cell !== 'miss' && cell !== 'ocean')
-				tableHtml += '<td class="ship"></td>';
+				tableHtml += '<td class="ocean"></td>';
 			else if (cell === 'hit')
 				tableHtml += '<td class="hit"></td>';
 			else if (cell === 'miss')
@@ -43,12 +47,12 @@ function updateComputerTable() {
 	tableHtml += '</table>';
 
 	div.innerHTML = tableHtml;
-	makeTableInteractive(players.computer.table);
+	makeTableInteractive('computer');
 }
 
 function updateUserTable() {
-	let div = document.getElementById('user');
-	let tableHtml = '<table class="table" id ="user">';
+	let div = document.getElementById('user-grid');
+	let tableHtml = '<table class="table" align="center" id ="user">';
 
 	for (let i = 0; i < WIDTH; i++) {
 		tableHtml += '<tr>';
@@ -70,74 +74,16 @@ function updateUserTable() {
 	tableHtml += '</table>';
 
 	div.innerHTML = tableHtml;
-	makeTableInteractive();
 }
 
-// function updateCoordinatesDisplay(row, col) {
-// 	let coordinatesDisplay = document.getElementById('coordinates-display');
-// 	coordinatesDisplay.innerHTML = `<p class="container">(${row + 1} , ${col + 1})</p>`;
-//
-// }
+function play() {
+	let userName = document.getElementById('username').value;
+	localStorage.setItem('username', userName);
+	document.location.href = 'views/game.html';
+}
 
-// function displayUserInput(shipName) {
-// 	let playerName = document.getElementById('player-name').value;
-// 	let difficulty = '';
-// 	let difficultyBoxes = document.getElementsByClassName('difficulty-checkbox');
-//
-// 	for (let i = 0; difficultyBoxes[i]; i++) {
-// 		if (difficultyBoxes[i].checked) {
-// 			difficulty = difficultyBoxes[i].value;
-// 			break;
-// 		}
-// 	}
-//
-// 	alert(`Players name is ${playerName}. \nDifficulty: ${difficulty} \nShip to be placed: ${shipName}`)
-// }
 
-// function login() {
-// 	const username = document.getElementById('username').value;
-// 	const password = document.getElementById('password').value;
-// 	const data = `userName=${username}&password=${password}`;
-// 	const localRequest = new XMLHttpRequest();
-//
-// 	localRequest.open('POST', 'http://universe.tc.uvu.edu/cs2550/assignments/PasswordCheck/check.php', false);
-// 	localRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-// 	localRequest.send(data);
-//
-// 	console.log(localRequest.status);
-//
-// 	if (localRequest.status === 200) {
-// 		let response = JSON.parse(localRequest.responseText);
-//
-// 		if(response.result === 'valid'){
-// 			let loginInfo = `${response.userName} ${response.timestamp}`;
-// 			localStorage.setItem('cs2550timestamp', loginInfo);
-// 			document.location.href = '../views/game.html';
-//
-// 		}
-// 		else{
-// 			document.getElementById('wrong-password').style.display = "inherit";
-// 		}
-//
-// 	}
-// 	else {
-// 		document.getElementById('wrong-password').style.display = "inherit";
-// 		document.getElementById('wrong-password').innerHTML = "Can't connect to server!";
-// 	}
-// }
-
-// function getUserInfo() {
-// 	let userInfo = localStorage.getItem('cs2550timestamp');
-// 	if(userInfo){
-// 		document.getElementById('user-info').innerHTML = userInfo;
-// 	}
-// 	else{
-// 		document.getElementById('user-info').innerHTML = 'Nothing in Local Storage!';
-// 	}
-//
-// }
-
-// function clearStorage() {
-// 	localStorage.clear();
-// 	getUserInfo()
-// }
+function clearStorage() {
+	localStorage.clear();
+	document.getElementById('playerName').innerHTML = "No player name in local storage";
+}
--- js/model.js
+++ js/model.js
@@ -6,23 +6,38 @@ const miss = 'miss';
 let request;
 let players;
 
-//get JSON OBject
+let finder = {
+	continue: false,
+	cellToBeChecked: {
+		row: 0,
+		col: 0,
+	},
+	originalCell: {
+		row: 0,
+		col: 0,
+	},
+	grid: {
+		up: true,
+		down: true,
+		left: true,
+		right: true,
+	}
+};
+
 
 function getGameData() {
 	request = new XMLHttpRequest();
 	request.onreadystatechange = function () {
 		if (this.readyState === 4 && this.status === 200) {
 			players = JSON.parse(this.responseText);
-			createTable(10,10);
+			createTable(10, 10);
 		}
 	};
 	request.open("GET", "../js/game.json", true);
 	request.send();
-
 }
 
 function createTableArray(height, width) {
-	// Will be implemented when not showing current game state.
 	let columns = [];
 
 	for (let i = 0; i < height; i++) {
@@ -38,16 +53,26 @@ function createTableArray(height, width) {
 	players.user.table = columns;
 }
 
-function missShip(col, row, player) {
+function placeShips(shipName, orientation, coordinates) {
+
+}
+
+//===============================//
+//==========Cell Actions=========//
+//===============================//
+
+
+function missShip(row, col, player) {
 	players[player].table[row][col] = miss;
+	if (player === 'user') {
+		updateUserTable();
+	}
+	else {
+		updateComputerTable();
+	}
 }
 
-function placeShip(shipName, orientation, coordinates) {
-	//TODO: Create funciton that places the ship the user chooses
-	//TODO: and where they choose it to be
-}
-
-function hitShip(col, row, player) {
+function hitShip(row, col, player) {
 	let table = players[player].table;
 	let cell = table[row][col];
 	players[player].ships.forEach((ship) => {
@@ -56,43 +81,251 @@ function hitShip(col, row, player) {
 			ship.hits++;
 
 			if (ship.hits === ship.maxHits) {
-				alert(`You have sunken a ${ship.type}!`)
+				if (player === 'user') {
+					alert(`${localStorage.getItem('username')}'s ${ship.type} has been destroyed!`);
+					ship.destroyed++;
+				}
+				else {
+					alert(`${player}'s ${ship.type} has been destroyed!`);
+					ship.destroyed++;
+				}
 			}
+
 			table[row][col] = hit;
 		}
-	})
+	});
+
+	//Check to see if the user has won the game
+	if (players[player].destroyedShips === players[player].totalShips) {
+		if (player === 'user') {
+			updateUserTable();
+			alert(`${localStorage.getItem('username')}' has won the game!!`);
+		}
+		else {
+			alert(`${player}'s ${ship.type} has won the game!!!`);
+			updateComputerTable();
+		}
+	}
+
+	//Update the grid
+	if (player === 'user') {
+		updateUserTable();
+	}
+	else {
+		updateComputerTable();
+	}
 }
 
-function checkIfHit(col, row, player) {
-	let cell = players[player].table[row][col];
+//=======================================//
+//========Checking Computer Grid=========//
+//=======================================//
+
+
+function checkComputer(row, col) {
+	let cell = players.computer.table[row][col];
 	if (cell !== 'ocean' && cell !== 'miss' && cell !== 'hit') {
-		hitShip(col, row, player);
+		hitShip(row, col, 'computer');
 	}
 	else if (cell === ocean) {
-		missShip(col, row, player)
+		missShip(row, col, 'computer');
 	}
 	else if (cell === hit) {
-		alert('You already hit that part of the ship!')
+		alert('You already hit that part of the ship!');
+		return 'retry';
 	}
 	else if (cell === miss) {
-		alert('You already checked that part of the ocean!')
+		alert('You already checked that part of the ocean!');
+		return 'retry';
 	}
 }
 
 function checkCoordinates() {
 	let col = this.cellIndex;
 	let row = this.parentNode.rowIndex;
-	let player = this.parentElement.parentNode.parentNode.id;
 
-	checkIfHit(col, row, player);
+	let result = checkComputer(row, col);
 
-	if (player === 'computer') {
-		updateComputerTable();
+	// Computers Turn
+	if (result !== 'retry') {
+		findUsersShips();
+	}
+}
+
+
+//==============================================//
+//================Computer AI===================//
+//==============================================//
+
+
+//===========Check User's Grid==================//
+
+function checkUserCell(row, col) {
+	let cell;
+
+	if (row >= 0 && row !== 10 && col >= 0 && col !== 10) {
+		cell = players.user.table[row][col];
+		if (cell !== 'ocean' && cell !== 'miss' && cell !== 'hit') {
+			hitShip(row, col, 'user');
+			return 'hit';
+		}
+		else if (cell === ocean) {
+			missShip(row, col, 'user');
+			return 'miss';
+		}
+		else if (cell === hit) {
+			return 'alreadyHit'
+		}
+		else if (cell === miss) {
+			return 'alreadyMissed';
+		}
 	}
 	else {
-		updateUserTable();
+		return 'outOfBounds';
 	}
 
 }
 
-getGameData();
+function findUsersShips() {
+	let checkedResult = '';
+	let row = Math.round(Math.random() * 9);
+	let col = Math.round(Math.random() * 9);
+
+	if (!finder.continue) {
+		finder.originalCell.row = row;
+		finder.originalCell.col = col;
+		finder.cellToBeChecked.row = row;
+		finder.cellToBeChecked.col = col;
+
+		checkedResult = checkUserCell(row, col);
+
+		if (checkedResult === 'alreadyMissed') {
+			return findUsersShips();
+		} else if (checkedResult === 'miss') {
+
+			return;
+		}
+		else if (checkedResult === 'hit') {
+			finder.continue = true;
+
+			return;
+		}
+	}
+
+	return checkNextSquare();
+}
+
+//Logic to continue searching once Hit//
+
+function resetCellToBeChecked() {
+	finder.cellToBeChecked.row = finder.originalCell.row;
+	finder.cellToBeChecked.col = finder.originalCell.col;
+}
+
+
+function checkNextSquare() {
+	if (finder.grid.up) {
+		return search.up();
+	}
+	else if (finder.grid.down) {
+		return search.down();
+	}
+	else if (finder.grid.left) {
+		return search.left();
+	}
+	else if (finder.grid.right) {
+		return search.right();
+	}
+	else {
+		finder.grid.up = true;
+		finder.grid.down = true;
+		finder.grid.left = true;
+		finder.grid.right = true;
+		finder.continue = false;
+	}
+}
+
+const search = {
+	up: function () {
+		finder.cellToBeChecked.row -= 1;
+		let result = checkUserCell(finder.cellToBeChecked.row, finder.cellToBeChecked.col);
+
+		if (result === 'outOfBounds' || result === 'alreadyMissed') {
+			finder.grid.up = false;
+			resetCellToBeChecked();
+			return checkNextSquare();
+		}
+		else if (result === 'miss') {
+			finder.grid.up = false;
+			return resetCellToBeChecked();
+		}
+		else if (result === 'alreadyHit') {
+			finder.grid.up = false;
+			resetCellToBeChecked();
+			return checkNextSquare();
+		}
+
+	},
+	down: function () {
+		finder.cellToBeChecked.row += 1;
+		let result = checkUserCell(finder.cellToBeChecked.row, finder.cellToBeChecked.col);
+
+		if (result === 'outOfBounds' || result === 'alreadyMissed') {
+			finder.grid.down = false;
+			resetCellToBeChecked();
+			return checkNextSquare();
+		}
+		else if (result === 'miss') {
+			finder.grid.down = false;
+			return resetCellToBeChecked();
+		}
+		else if (result === 'alreadyHit') {
+			finder.grid.down = false;
+			resetCellToBeChecked();
+			return checkNextSquare();
+		}
+	},
+	right: function () {
+		finder.cellToBeChecked.col += 1;
+		let result = checkUserCell(finder.cellToBeChecked.row, finder.cellToBeChecked.col);
+
+		if (result === 'outOfBounds' || result === 'alreadyMissed') {
+			finder.grid.right = false;
+			resetCellToBeChecked();
+			return checkNextSquare();
+		}
+		else if (result === 'miss') {
+			finder.grid.right = false;
+			return resetCellToBeChecked();
+		}
+		else if (result === 'alreadyHit') {
+			finder.grid.right = false;
+			resetCellToBeChecked();
+			return checkNextSquare();
+		}
+	},
+	left: function () {
+		finder.cellToBeChecked.col -= 1;
+		let result = checkUserCell(finder.cellToBeChecked.row, finder.cellToBeChecked.col);
+
+		if (result === 'outOfBounds' || result === 'alreadyMissed') {
+			finder.grid.left = false;
+			resetCellToBeChecked();
+			return checkNextSquare();
+		}
+		else if (result === 'miss') {
+			finder.grid.left = false;
+			return resetCellToBeChecked();
+		}
+		else if (result === 'alreadyHit') {
+			finder.grid.left = false;
+			resetCellToBeChecked();
+			return checkNextSquare();
+		}
+	}
+};
+
+// function run() {
+// 	checkUserCell(5, -1)
+// }
+//
+// run();
